7791ffefff1e654b60aa3330756a1bfc35bc68ac
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-19 16:53:42 +0100

    add neem example

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,13 +4,13 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
-# Start MongoDB
-mongod --fork --logpath ${PWD}/mongod.log
+# Start MongoDB and save data on working directory
+MONGODB_URL=mongodb://127.0.0.1:27017
+mkdir -p ${PWD}/mongodb/data
+mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
 
 # Launch Knowrob
-source ${KNOWROB_WS}/devel/setup.bash
-
-export KNOWROB_MONGODB_URI="mongodb://127.0.0.1:27017/?appname=knowrob"
+export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob knowrob.launch &
 
 exec "$@"
