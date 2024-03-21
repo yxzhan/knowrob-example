a1ae141a21509dffdb8a73da6141a54e789cd186
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-21 15:16:14 +0100

    create symbolic link of the neem_data in the entrypoint

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -9,6 +9,9 @@ MONGODB_URL=mongodb://127.0.0.1:27017
 mkdir -p ${PWD}/mongodb/data
 mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
 
+# Create a symbolic link to the folder neem_data
+ln -s /neem_data ${PWD}/neem_data
+
 # Launch Knowrob
 export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob knowrob.launch &
